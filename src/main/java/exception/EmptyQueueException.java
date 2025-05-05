35ea8387aa0fd7b9e4c20264660cd6aefbb81368
katekiselevich <kiselkath@gmail.com>
2025-05-05 16:33:03 +0200

    Fixed func startRide, added exception

---
+++ src/main/java/exception/EmptyQueueException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class EmptyQueueException extends RuntimeException {
+    public EmptyQueueException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ride/RideQueueManager.java
+++ src/main/java/ride/RideQueueManager.java
@@ -1,11 +1,14 @@
 package ride;
 
+import exception.EmptyQueueException;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class RideQueueManager {
-    private Queue<Guest> guestQueue;
+    private final Queue<Guest> guestQueue;
 
     public RideQueueManager(){
         guestQueue = new LinkedList<>();
@@ -23,8 +26,16 @@ public class RideQueueManager {
      * Processes the ride for guest in queue and removes guest from the line
      * @return the processed ride, or null if the queue is empty
      */
-    public Guest startRide(){
-        return this.guestQueue.poll();
+    public List<Guest> startRide(int rideCapacity){
+        if (guestQueue.isEmpty()) {
+            throw new EmptyQueueException("Cannot start ride: guest queue is empty.");
+        }
+
+        List<Guest> riders = new ArrayList<>();
+        for (int i = 0; i < rideCapacity && !guestQueue.isEmpty(); i++) {
+            riders.add(guestQueue.poll());
+        }
+        return riders;
     }
 
     /**
