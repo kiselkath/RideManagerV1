73adea17ca54f7e23ca355011ad917bd63870a5f
katekiselevich <kiselkath@gmail.com>
2025-05-05 17:28:27 +0200

    Fixed RideHistoryStack

--- src/main/java/ride/RideHistoryStack.java
+++ src/main/java/ride/RideHistoryStack.java
@@ -1,38 +1,32 @@
 package ride;
 
-import sun.jvm.hotspot.tools.JStack;
-
 import java.util.Stack;
 
 public class RideHistoryStack {
     private final Stack<Guest> saveRide;
 
-    public RideHistoryStack(Stack<Guest> saveRide) {
+    public RideHistoryStack() {
         this.saveRide = new Stack<>();
     }
 
     /**
      * push(E item) - Добавляет элемент на вершину стека
      */
-    public addRide(){
-        return this.saveRide.add(Guest);
+    public void addRide(Guest guest) {
+        this.saveRide.push(guest);
     }
 
     /**
      * peek() - Просто возвращает элемент с вершины без удаления
      */
-    public viewLastRide() {
-        return (this.saveRide.isEmpty()) ? null : this.saveRide.peek();
-
+    public Guest viewLastRide() {
+        return this.saveRide.isEmpty() ? null : this.saveRide.peek();
     }
-
 
     /**
      * pop() - Удаляет и возвращает элемент с вершины стека (LIFO)
      */
-    public removeLastRider() {
-        return (this.saveRide.isEmpty()) ? null : this.saveRide.pop();
+    public Guest removeLastRider() {
+        return this.saveRide.isEmpty() ? null : this.saveRide.pop();
     }
-
-
 }
